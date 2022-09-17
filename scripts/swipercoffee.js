0bbdde67aefda0cdb40283495307cbc6c2eab6b9
Borodach <75586389+BoRoDaTeNkiY@users.noreply.github.com>
2022-09-17 23:27:15 +0300

    Add files via upload

---
+++ scripts/swipercoffee.js
@@ -0,0 +1,11 @@
+new Swiper('.image-slider',{
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+    }
+});
